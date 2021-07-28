556a9122194ab1ec1a6003278b7bb75204d2d83b
MaryCrisEstudillo <estudillomarycris@gmail.com>
2021-07-28 13:09:25 +0800

    added post route for new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :watercolor_blogs
   root 'watercolor_blogs#index'
   get '/watercolor_blogs/show'
+  post '/watercolor_blogs/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
